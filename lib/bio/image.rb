d6ba06016652dc2f8ca45636b2ceb32a69671f1d
e-mka <michal.koziarski@netguru.pl>
2011-05-23 21:58:15 +0200

    Bio::Image - refactor

--- lib/bio/image.rb
+++ lib/bio/image.rb
@@ -1,19 +1,27 @@
 module Bio
   class Image
-    private
+    DEFAULT_HEIGHT = 300
+    DEFAULT_WIDTH = 300
+    ATTRIBUTES = [:title, :author, :date, :height, :width]
     
-    def self.attributes
-      [:title, :author, :date, :height, :width]
+    attr_accessor :dataset, *ATTRIBUTES
+    
+    def initialize dataset, options = {}
+      @dataset = dataset
+      
+      ATTRIBUTES.each { |attr| send("#{attr.to_s}=", options[attr]) }
+      
+      set_default_options
     end
     
-    public
+    private
     
-    attr_accessor *self.attributes
-    
-    def initialize options = {}
-      @date = Time.now
+    def set_default_options
+      @date ||= Time.now
+      @height ||= DEFAULT_HEIGHT
+      @width ||= DEFAULT_WIDTH
       
-      self.class.attributes.each {|attr| send("#{attr.to_s}=", options[attr]) if options[attr]}
+      # TODO : size must be enough to fit data
     end
   end
 end
--- spec/bio/image_spec.rb
+++ spec/bio/image_spec.rb
@@ -1,17 +1,21 @@
 require 'spec_helper'
 
 describe Bio::Image do
-  Bio::Image.send(:attributes).each do |attr|
-    it "should be possible to set #{attr} attribute on creating object" do
-      @image = Bio::Image.new attr.to_sym => 'value'
-      @image.send(attr.to_sym).should == 'value'
+  before(:each) do
+    @dataset = Statsample::Dataset.new(:v => [1, 2].to_scale)
+  end
+  
+  Bio::Image::ATTRIBUTES.each do |attr|
+    it "should be possible to set #{attr} attribute on creation" do
+      @image = Bio::Image.new @dataset, attr => 'value'
+      @image.send(attr).should == 'value'
     end
   end
   
   it "should set current time on creation" do
     Timecop.freeze
     
-    @image = Bio::Image.new
+    @image = Bio::Image.new @dataset
     @image.date.should == Time.now
   end
 end
