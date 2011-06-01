b8976ac0b8066c3352155dc93be40a6c6839e34d
e-mka <michal.koziarski@netguru.pl>
2011-06-01 19:29:25 +0200

    modified including visualzation methods

--- lib/bio.rb
+++ lib/bio.rb
@@ -1,16 +1,18 @@
 module Bio
   
-  # TODO : bellow code doesn't work, it is executed only once, in the module itsef.
-  # Find a way of executing it every time it's included
-  
-  if self.respond_to? :to_dataset
-    file_names = Dir.entries("#{File.dirname(__FILE__)}/bio/image").select {|file_name| file_name =~ /\.rb/}
-    classes = file_names.collect {|file_name| file_name.slice! ".rb"}
-    
-    classes.each do |c|
-      define_method "to_#{c}" do |options = {}|
-        eval("Bio::Image::#{c.capitalize}").new(self.to_dataset, options)
+  def initialize  
+    if self.respond_to? :to_dataset
+      file_names = Dir.entries("#{File.dirname(__FILE__)}/bio/image").select {|file_name| file_name =~ /\.rb/}
+      classes = file_names.collect {|file_name| file_name.chomp ".rb"}
+      
+      classes.each do |c|
+        unless self.respond_to? "to_#{c}"
+          self.class.send :define_method, "to_#{c}" do |options = {}|
+            eval("Bio::Image::#{c.capitalize}").new(self.to_dataset, options)
+          end
+        end
       end
+      
     end
   end
   
