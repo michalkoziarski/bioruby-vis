98f93705cf8febdb5111fbcf8deea66e30b82215
e-mka <michal.koziarski@netguru.pl>
2011-06-01 22:03:49 +0200

    visualize method

--- lib/bio.rb
+++ lib/bio.rb
@@ -1,19 +1,32 @@
 module Bio
   
-  def initialize  
-    if self.respond_to? :to_dataset
-      file_names = Dir.entries("#{File.dirname(__FILE__)}/bio/image").select {|file_name| file_name =~ /\.rb/}
-      classes = file_names.collect {|file_name| file_name.chomp ".rb"}
+  DEFAULT_VISUALIZE_METHOD = :to_dataset
+  
+  def self.included(base)
+    base.extend(ClassMethods)
+  end
+  
+  module ClassMethods
+  
+    def visualize options = {}
+      [:except, :only].each do |option|
+        options[option] ||= []      
+        options[option] = [options[option]] unless options[option].class == Array
+      end
       
-      classes.each do |c|
-        unless self.respond_to? "to_#{c}"
-          self.class.send :define_method, "to_#{c}" do |options = {}|
-            eval("Bio::Image::#{c.capitalize}").new(self.to_dataset, options)
-          end
+      visualize_method = options[:with] || DEFAULT_VISUALIZE_METHOD
+    
+      file_names = Dir.entries("#{File.dirname(__FILE__)}/bio/image").select {|file_name| file_name =~ /\.rb/}
+      klasses = file_names.collect {|file_name| file_name.chomp ".rb"}
+      
+      klasses.each do |klass|
+        next if options[:except].include? klass.to_sym or (!options[:only].empty? and !options[:only].include? klass.to_sym)
+        
+        self.send :define_method, "to_#{klass}" do |options = {}|
+          eval("Bio::Image::#{klass.capitalize}").new(self.send(visualize_method), options)
         end
       end
       
     end
   end
-  
 end
