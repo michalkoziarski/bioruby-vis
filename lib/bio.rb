13fb65cbdeac7ffae42d4df9712cbd61b1da8817
e-mka <michal.koziarski@netguru.pl>
2011-06-01 18:57:44 +0200

    method_definied? changed to respond_to?

--- lib/bio.rb
+++ lib/bio.rb
@@ -3,7 +3,7 @@ module Bio
   # TODO : bellow code doesn't work, it is executed only once, in the module itsef.
   # Find a way of executing it every time it's included
   
-  if method_defined? :to_dataset
+  if self.respond_to? :to_dataset
     file_names = Dir.entries("#{File.dirname(__FILE__)}/bio/image").select {|file_name| file_name =~ /\.rb/}
     classes = file_names.collect {|file_name| file_name.slice! ".rb"}
     
