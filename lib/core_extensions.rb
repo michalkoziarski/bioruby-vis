003ad1e456e540f6aba63601a37b186961eb67c9
e-mka <michal.koziarski@netguru.pl>
2011-08-29 17:41:50 +0200

    easier image creating for datasets

--- lib/core_extensions.rb
+++ lib/core_extensions.rb
@@ -7,3 +7,13 @@ class Array
     Statsample::Dataset.new(:x => (0...self.size).to_a.to_scale, :y => self.to_scale)
   end
 end
+
+class Statsample::Dataset
+  include Bio
+  
+  visualize
+  
+  def to_dataset
+    self
+  end
+end
